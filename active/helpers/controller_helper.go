8d1531bd6c139bc2d1828f8f8f6e404f18809f8b
Charles Washington <charles_was_7@hotmail.com>
2019-06-26 17:58:26 -0300

    Add a new helper for Iris controllers

--- active/helpers/controller_helper.go
+++ active/helpers/controller_helper.go
@@ -1,6 +1,8 @@
 package helpers
 
-import "github.com/kataras/iris"
+import (
+	"github.com/kataras/iris"
+)
 
 func Slicefy(ctx iris.Context, keys ...string) (keyValue []interface{}) {
 	for _, key := range keys {
@@ -9,4 +11,19 @@ func Slicefy(ctx iris.Context, keys ...string) (keyValue []interface{}) {
 	}
 
 	return keyValue
+}
+
+func GetUrlParams(ctx iris.Context) (urlParams map[string]string) {
+	urlParams = ctx.URLParams()
+	delete(urlParams, "offset")
+	delete(urlParams, "pagesize")
+
+	return urlParams
+}
+
+func GetPaginationValues(ctx iris.Context) (offset, pagesize int) {
+	offset, _ = ctx.URLParamInt("offset")
+	pagesize, _ = ctx.URLParamInt("pagesize")
+
+	return offset, pagesize
 }
